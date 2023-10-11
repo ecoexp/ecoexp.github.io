617473d8de663c8adf0c462a24b230ddbfdbba1d
ecoexp <105444997+ecoexp@users.noreply.github.com>
2023-10-11 17:27:29 +0200

    Update script.js

--- jss/script.js
+++ jss/script.js
@@ -79,8 +79,8 @@ newuserdata.set({
     
     const links = ['00', '10'];
     const linkIndex = Math.floor(Math.random() * links.length);
-    localStorage.setItem('a_group', links[linkIndex] );
-    //localStorage.setItem('a_group', '10' );
+    //localStorage.setItem('a_group', links[linkIndex] );
+    localStorage.setItem('a_group', '00' );
     const linkToOpen='Pre-study/index.html'
     window.location.href = linkToOpen;
 
